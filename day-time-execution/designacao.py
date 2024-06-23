81b4ca576593876ffbf58abea118b88d3bbb5a85
=Antony <guilhermecosta994@gmail.com>
2024-06-23 11:36:43 -0300

    Experimentos 60 6 21

--- day-time-execution/designacao.py
+++ day-time-execution/designacao.py
@@ -24,8 +24,10 @@ TEMPO_ENTRE_SIMULACOES = 5
 
 # Experimentos
 # variar a quantidade de ambulâncias e velocidade
-# Velocidade    | Ambulâncias Avançadas     | Ambulâncias Avançadas
-# 50km/h        | 6                         | 21
+# Velocidade    | Ambulâncias Avançadas     | Ambulâncias Avançadas | Done
+# 50km/h        | 6                         | 21                    | X
+# 60km/h        | 6                         | 21                    | 
+# 40km/h        | 6                         | 21                    | 
 
 todosOsAtendimentosAvancadosNaoRealizadosByUID : Dict[str, float] = {}
 todosOsAtendimentosBasicosNaoRealizadosByUID : Dict[str, float] = {}
